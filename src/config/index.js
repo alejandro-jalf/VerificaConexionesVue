ab252c2e8ad95cd77bfbf6c336db35c6b873ec4c
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-10-23 10:30:37 -0500

    ADD: Crea pseudousuarios para pruebas de logueo

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,19 @@
+const users = [
+    {
+        nameUser: process.env.VUE_APP_USER1,
+        password: process.env.VUE_APP_PASSWORD1,
+        privilegios: "superusuario",
+        accessTo: ["Home", "About", "Login", "Conexiones"]
+    },
+    {
+        nameUser: process.env.VUE_APP_USER2,
+        password: process.env.VUE_APP_PASSWORD2,
+        privilegios: "invitado",
+        accessTo: ["About"]
+    }
+]
 module.exports = {
     urlConexion: process.env.VUE_APP_URL_API,
-    namesSucursales: JSON.parse(process.env.VUE_APP_NAME_SUCURSALES)
+    namesSucursales: JSON.parse(process.env.VUE_APP_NAME_SUCURSALES),
+    users
 }
